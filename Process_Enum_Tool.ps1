6ef42b32518f82b1cecbf7cd1ecb93b079ec039f
TC Johnson <superteece@keemail.me>
2015-09-24 22:44:21 -0700

    Enhancements
    Added validation of process signature

--- Process_Enum_Tool.ps1
+++ Process_Enum_Tool.ps1
@@ -1,7 +1,27 @@
 ﻿#Tool for enumerating processes via WIndows Powershell
+
+function main
+{
+    "Select a tool"
+    "1. List Running Processes"
+    "2. List Running Processes by Company"
+    "3. Validate a process' signature"
+    ""
+    $response = Read-Host "Selection: "
+
+        if ($response -eq 1)
+            {list}
+        elseif ($response -eq 2)
+            {listbycomp}
+        elseif ($response -eq 3)
+            {validate}
+}
+
+
 function list 
 {
     Get-Process
+    end
 }
 
 function listbycomp
@@ -19,6 +39,33 @@ function listbycomp
         ""
         $c = Read-Host "Where would you like to save your file?"
         Get-Process | Group-Object company | Sort-Object name | Export-Csv -path $c$b.csv}
+    end
+}
+
+function validate
+{
+    $procName = Read-Host "Enter the name of the process to validate: "
+    $procValidate = Get-Process $procName | select -Expand Path | select -Unique
+    Get-AuthenticodeSignature "$procValidate"
+    end
+}
+
+function end
+{
+    "Whould you like to:"
+    "1. Return to Main Menu"
+    "2. Exit"
+    $end =  Read-Host "Selection: "
+        if ($end = 1)
+            {main}
+        elseif ($end = 2)
+            {exit}
+}
+
+function bye
+{
+    "Good Bye"
+    
 }
 
 
@@ -29,14 +76,17 @@ function listbycomp
 " | |   | | | (_) | (_|  __/\__ \__ \ | |____| | | | |_| | | | | | |"
 " |_|   |_|  \___/ \___\___||___/___/ |______|_| |_|\__,_|_| |_| |_|"
 ""
-"Select a tool"
-"1. List Running Processes"
-"2. List Running Processes by Company"
-""
-$response = Read-Host "Selection: "
 
-if ($response -eq 1)
-    {list}
-elseif ($response -eq 2)
-    {listbycomp}
+    "Select a tool"
+    "1. List Running Processes"
+    "2. List Running Processes by Company"
+    "3. Validate a process' signature"
+    ""
+    $response = Read-Host "Selection: "
 
+        if ($response -eq 1)
+            {list}
+        elseif ($response -eq 2)
+            {listbycomp}
+        elseif ($response -eq 3)
+            {validate}
